c5ea318c2d9f4df3cb3f42fc5a70937f09a6fee0
Stanislav Sopov <stassop@gmail.com>
2019-04-11 14:46:47 +0200

    Changed main.js to index.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './index.js',
   mode: 'development',
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
