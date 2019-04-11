39ffb473256a5a200a907620ee68cc238f23a332
Stanislav Sopov <stassop@gmail.com>
2019-04-11 15:07:54 +0200

    Changed index.js back to main.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  entry: './main.js',
   mode: 'development',
   output: {
-    filename: 'index.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
